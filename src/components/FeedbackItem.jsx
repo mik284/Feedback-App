bcec8c1210bd7d2f3084bd9e936675b0ce304fa7
mik284 <mikeykamau222@gmail.com>
2022-08-17 18:34:53 +0300

    Card Componet & Conditional Styles and class

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,6 @@
 // import {useState} from 'react'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
 
 function FeedbackItem({item}) {
     // const [rating, setRating] = useState(7)
@@ -10,12 +12,18 @@ function FeedbackItem({item}) {
     // }
 
   return (
-    <div className="card">
+    <Card>
         <div className="num-display">{item.rating}</div>
         <div className="text-display">{item.text}</div>
         {/* <button onClick={handleClick}>Click</button> */}
-    </div>
+    </Card>
   )
+}
+
+
+
+FeedbackItem.propTypes={
+    item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
