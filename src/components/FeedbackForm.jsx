040a76832e20806011e3832c99518548d183983d
mik284 <mikeykamau222@gmail.com>
2022-08-18 17:23:43 +0300

    Real time Validation using STATES

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,9 +5,23 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
     const [text, setText] = useState("")
+    const [btnDisabled, setBtnDisabled] = useState(true) // disable button if text is empty
+    const [message, setMessage] = useState("")
 
     const handleTextChange = (e) => {
-        setText(e.target.value)
+
+        if(text === "") {
+            setBtnDisabled(true) // disable button if text is empty
+            setMessage(null)
+            } else if(text !== "" && text.trim().length > 0) { // enable button if text is not empty
+                setMessage("Text must be atleast 10 characters long") // message is set to null if text is empty
+                setBtnDisabled(false) // Enable the button
+            } else {
+                setMessage("null") // message is set to null if text is empty
+                setBtnDisabled(false) // Enable the button
+            }
+
+        setText(e.target.value) // set the text to the value of the input
     }
 
   return (
@@ -17,8 +31,10 @@ function FeedbackForm() {
             {/* @todo - rating select component */}
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder="write a review" value={text}/>
-                <Button type="submit">Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+
+            {message && <div className="message">{message}</div>}
         </form>
     </Card>
   )
