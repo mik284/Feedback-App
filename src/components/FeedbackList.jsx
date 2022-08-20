c5459f5b52756e15cd5036a8380b62ce5211c9ac
mik284 <mikeykamau222@gmail.com>
2022-08-20 20:51:24 +0300

    Added some animation using FRAMER motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
 
@@ -7,12 +8,25 @@ function FeedbackList({ feedback, handleDelete}) {
     }
   return (
     <div className="feedback-List">
-        {feedback.map((item) => (
-            <FeedbackItem 
-            key={item.id} 
-            item={item} 
-            handleDelete= {handleDelete}/>
-        ))}
+        <AnimatePresence>
+            {feedback.map((item) => (
+
+                <motion.div
+                     key={item.id}
+                     initial={{opacity: 0  }}
+                     animate={{opacity: 1  }}
+                     exit={{opacity: 0  }}
+                >
+                     <FeedbackItem 
+                        key={item.id} 
+                        item={item} 
+                        handleDelete= {handleDelete}/>
+                </motion.div>
+               
+            ))}
+
+        </AnimatePresence>
+        
     </div> 
   )
 }
