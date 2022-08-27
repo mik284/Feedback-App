df55b7181e9d09a0770bfe0e075f420cdd7170c2
mik284 <mikeykamau222@gmail.com>
2022-08-27 20:47:46 +0300

    created Routes using React v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import FeedbackList from "./components/FeedbackList"
 import Header from "./components/Header"
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
-
+import AboutPage from './pages/AboutPage'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -45,14 +46,25 @@ if(window.confirm('Are you sure you want to delete?')){
         if (loading) return <h1>Loading...</h1> */
 
     return (
-        <>
-        <Header />
-            <div className='container'>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback}/>
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />          
-            </div>
-        </>
+        <Router>
+            <Header />
+                <div className='container'>
+                    <Routes>
+                        <Route exact path='/' element={
+                            <>
+                                <FeedbackForm handleAdd={addFeedback} />
+                                <FeedbackStats feedback={feedback}/>
+                                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />   
+                            </>
+                        }>
+                            
+                        </Route>
+                        
+
+                        <Route path='/about' element={<AboutPage/>}/>   
+                    </Routes>  
+                </div>
+        </Router>
         
 
     )
